753cea51839f8f279485e717adb71650a2944ae4
carlos_ <carlos.condori1412@gmail.com>
2022-08-23 23:47:17 -0300

    agregando genero e implementando carrito

--- codigo.js
+++ codigo.js
@@ -2,6 +2,7 @@ let peliculas=[
     {
         id:1,
         precio:100,
+        generos:["superheroes","violencia","accion","serie"],
         nombre:"WATCHMEN",
         descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
         portada:"./portadas/img1-Watchmen.jpg",
@@ -11,6 +12,7 @@ let peliculas=[
     {
         id:2,
         precio:200,
+        generos:["serie","catastrofes","hechos reales"],
         nombre:"CHERNOBYL",
         descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
         portada:"./portadas/img2-Chernobyl.jpg",
@@ -19,6 +21,7 @@ let peliculas=[
     {
         id:3,
         precio:150,
+        generos:["animacion","superheroes","misterio"],
         nombre:"BATMAN",
         descripcion:"El equipo : la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
         portada:"./portadas/img3-Batman.jpg",
@@ -27,6 +30,7 @@ let peliculas=[
     {
         id:4,
         precio:190,
+        generos:["ciencia ficcion","drama","serie","accion"],
         nombre:"WESTWORLD",
         descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
         portada:"./portadas/img4-Westworld.jpg",
@@ -35,6 +39,7 @@ let peliculas=[
     {
         id:5,
         precio:220,
+        generos:["animacion","superheroes","aventura"],
         nombre:"SUPERMAN",
         descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
         portada:"./portadas/img5-Superman.jpg",
@@ -45,6 +50,7 @@ let peliculas=[
 function mostrar(){
     const contenedor = document.getElementById("listado");
         for(let laPelicula in peliculas){
+            let arraygeneros=peliculas[laPelicula].generos.join(" - ");
             contenedor.innerHTML+=
             `<div class="col-lg-4 col-sm-6 col-12">
                     <div class="card mt-3">
@@ -57,18 +63,46 @@ function mostrar(){
                             <img src=${peliculas[laPelicula].portada} class="card-img" alt="pelicula">
                             
                             <div>
-                            <br>
-                            <h2><strong>$${peliculas[laPelicula].precio}</strong></h2>
-    
-                            <br>
-                                <button type="button" class="btn btn-outline-success"><a target>COMPRAR</a></button>
-                                <a href="${peliculas[laPelicula].enlace}" class="btn btn-outline-warning" target="_blank">VER TRAILER</a>
+                                <br>
+                                    <h2><strong>$${peliculas[laPelicula].precio}</strong></h2>
+                                <br>
+                                    <button type="button" class="btn btn-outline-success" id="boton${peliculas[laPelicula].id}"><a target>COMPRAR</a></button>
+                                    <a href="${peliculas[laPelicula].enlace}" class="btn btn-outline-warning" target="_blank">VER TRAILER</a>
+                            </div>
+                            <div id="genero">
+                                <br>
+                                <h5><strong>${arraygeneros}<strong></h5>
+
                             </div>
                         </div>
-                    </div>
             </div>`;
         }
+
+        peliculas.forEach(laPelicula=>{
+            document.getElementById("`boton${peliculas[laPelicula].id}`").addEventListener("click",function(){
+                comprarPelicula(laPelicula);
+
+                console.log(`boton${peliculas[laPelicula].id}`);
+                console.log(`boton${laPelicula.id}`)
+
+            });
+        })
+
+}
+
+
+
+let carrito=[];
+let lista = document.getElementById("tablaCompras");
+
+function comprarPelicula(laPelicula){
+
+    carrito.push(laPelicula);
+    console.log("cargada");
 }
 
 mostrar();
 
+// for(let j=0;j<peliculas[laPelicula].generos.length;j++){
+            //     `<a>${(peliculas[laPelicula].generos[j])} sd</a>`
+            //
