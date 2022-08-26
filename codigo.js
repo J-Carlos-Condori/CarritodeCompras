4aaa5fc68767ea9c4bab35091d93fc8526708376
carlos_ <carlos.condori1412@gmail.com>
2022-08-26 14:44:16 -0300

    usando libreria sweetalert

--- codigo.js
+++ codigo.js
@@ -45,64 +45,63 @@ let peliculas=[
         portada:"./portadas/img5-Superman.jpg",
         enlace:"https://www.youtube.com/watch?v=ZSPwGK-xLhk",
     },
-]
+];
+
+
+
+
+
+let contenedor=document.getElementById("listado");
+
+mostrar();
 
 function mostrar(){
-    const contenedor = document.getElementById("listado");
-        for(let laPelicula in peliculas){
-            let arraygeneros=peliculas[laPelicula].generos.join(" - ");
-            contenedor.innerHTML+=
-            `<div class="col-lg-4 col-sm-6 col-12">
+        for (const laPelicula of peliculas){
+
+            let arraygeneros=laPelicula.generos.join(" - ");
+
+            contenedor.innerHTML+=`<div class="col-lg-4 col-sm-6 col-12">
                     <div class="card mt-3">
                         <div class="card-body">
-                            <h2 class="main__subtitulo card-tittle">${peliculas[laPelicula].nombre}</h2>
+                            <h2 class="main__subtitulo card-tittle">${laPelicula.nombre}</h2>
                             <div class="main__parrafo">
-                                <p class="card-text">${peliculas[laPelicula].descripcion}</p>
+                                <p class="card-text">${laPelicula.descripcion}</p>
                             </div>
                             <br>
-                            <img src=${peliculas[laPelicula].portada} class="card-img" alt="pelicula">
+                            <img src=${laPelicula.portada} class="card-img" alt="pelicula">
                             
                             <div>
                                 <br>
-                                    <h2><strong>$${peliculas[laPelicula].precio}</strong></h2>
+                                    <h2><strong>$${laPelicula.precio}</strong></h2>
                                 <br>
-                                    <button type="button" class="btn btn-outline-success" id="boton${peliculas[laPelicula].id}"><a target>COMPRAR</a></button>
-                                    <a href="${peliculas[laPelicula].enlace}" class="btn btn-outline-warning" target="_blank">VER TRAILER</a>
+                                    <button type="button" class="btn btn-outline-success" id="boton${laPelicula.id}"><a target>COMPRAR</a></button>
+                                    <a href="${laPelicula.enlace}" class="btn btn-outline-warning" target="_blank">VER TRAILER</a>
                             </div>
                             <div id="genero">
                                 <br>
                                 <h5><strong>${arraygeneros}<strong></h5>
-
                             </div>
                         </div>
             </div>`;
         }
-
         peliculas.forEach(laPelicula=>{
-            document.getElementById("`boton${peliculas[laPelicula].id}`").addEventListener("click",function(){
-                comprarPelicula(laPelicula);
-
-                console.log(`boton${peliculas[laPelicula].id}`);
-                console.log(`boton${laPelicula.id}`)
-
+            document.getElementById(`boton${laPelicula.id}`).addEventListener("click", function(){    
+                agregarPelicula(laPelicula)
             });
         })
-
 }
-
-
 
 let carrito=[];
-let lista = document.getElementById("tablaCompras");
 
-function comprarPelicula(laPelicula){
-
+function agregarPelicula(laPelicula){
     carrito.push(laPelicula);
-    console.log("cargada");
+    Swal.fire('agregaste <strong>'+laPelicula.nombre+"</strong> a tu compra");
+
+
+    document.getElementById("tablaCompras").innerHTML+=`
+        <td>${laPelicula.id}</td>
+        <td>${laPelicula.nombre}</td>
+        <td>${laPelicula.precio}$</td>
+    `;
 }
 
-mostrar();
-
-// for(let j=0;j<peliculas[laPelicula].generos.length;j++){
-            //     `<a>${(peliculas[laPelicula].generos[j])} sd</a>`
-            //
