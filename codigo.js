40d3901158e972c1f150708c366ef89d57f9a84b
carlos_ <carlos.condori1412@gmail.com>
2022-08-29 22:38:49 -0300

    correcciones

--- codigo.js
+++ codigo.js
@@ -1,49 +1,49 @@
-let peliculas=[
+let peliculas = [
     {
-        id:1,
-        precio:100,
-        generos:["superheroes","violencia","accion","serie"],
-        nombre:"WATCHMEN",
-        descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada:"./portadas/img1-Watchmen.jpg",
-        enlace:"https://www.youtube.com/watch?v=ejjFCjFJBXM",
+        id: 1,
+        precio: 100,
+        generos: ["superheroes", "violencia", "accion", "serie"],
+        nombre: "WATCHMEN",
+        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
+        portada: "./portadas/img1-Watchmen.jpg",
+        enlace: "https://www.youtube.com/watch?v=ejjFCjFJBXM",
 
     },
     {
-        id:2,
-        precio:200,
-        generos:["serie","catastrofes","hechos reales"],
-        nombre:"CHERNOBYL",
-        descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada:"./portadas/img2-Chernobyl.jpg",
-        enlace:"https://www.youtube.com/watch?v=s9APLXM9Ei8&t=31s",
+        id: 2,
+        precio: 200,
+        generos: ["serie", "catastrofes", "hechos reales"],
+        nombre: "CHERNOBYL",
+        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
+        portada: "./portadas/img2-Chernobyl.jpg",
+        enlace: "https://www.youtube.com/watch?v=s9APLXM9Ei8&t=31s",
     },
     {
-        id:3,
-        precio:150,
-        generos:["animacion","superheroes","misterio"],
-        nombre:"BATMAN",
-        descripcion:"El equipo : la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada:"./portadas/img3-Batman.jpg",
-        enlace:"https://www.youtube.com/watch?v=rrmUk2YUm14",
+        id: 3,
+        precio: 150,
+        generos: ["animacion", "superheroes", "misterio"],
+        nombre: "BATMAN",
+        descripcion: "El equipo : la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
+        portada: "./portadas/img3-Batman.jpg",
+        enlace: "https://www.youtube.com/watch?v=rrmUk2YUm14",
     },
     {
-        id:4,
-        precio:190,
-        generos:["ciencia ficcion","drama","serie","accion"],
-        nombre:"WESTWORLD",
-        descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada:"./portadas/img4-Westworld.jpg",
-        enlace:"https://www.youtube.com/watch?v=SV5lzrbNDx0",
+        id: 4,
+        precio: 190,
+        generos: ["ciencia ficcion", "drama", "serie", "accion"],
+        nombre: "WESTWORLD",
+        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
+        portada: "./portadas/img4-Westworld.jpg",
+        enlace: "https://www.youtube.com/watch?v=SV5lzrbNDx0",
     },
     {
-        id:5,
-        precio:220,
-        generos:["animacion","superheroes","aventura"],
-        nombre:"SUPERMAN",
-        descripcion:"El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada:"./portadas/img5-Superman.jpg",
-        enlace:"https://www.youtube.com/watch?v=ZSPwGK-xLhk",
+        id: 5,
+        precio: 220,
+        generos: ["animacion", "superheroes", "aventura"],
+        nombre: "SUPERMAN",
+        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
+        portada: "./portadas/img5-Superman.jpg",
+        enlace: "https://www.youtube.com/watch?v=ZSPwGK-xLhk",
     },
 ];
 
@@ -51,16 +51,16 @@ let peliculas=[
 
 
 
-let contenedor=document.getElementById("listado");
+let contenedor = document.getElementById("listado");
 
 mostrar();
 
-function mostrar(){
-        for (const laPelicula of peliculas){
+function mostrar() {
+    for (const laPelicula of peliculas) {
 
-            let arraygeneros=laPelicula.generos.join(" - ");
+        let arraygeneros = laPelicula.generos.join(" - ");
 
-            contenedor.innerHTML+=`<div class="col-lg-4 col-sm-6 col-12">
+        contenedor.innerHTML += `<div class="col-lg-4 col-sm-6 col-12">
                     <div class="card mt-3">
                         <div class="card-body">
                             <h2 class="main__subtitulo card-tittle">${laPelicula.nombre}</h2>
@@ -83,25 +83,24 @@ function mostrar(){
                             </div>
                         </div>
             </div>`;
-        }
-        peliculas.forEach(laPelicula=>{
-            document.getElementById(`boton${laPelicula.id}`).addEventListener("click", function(){    
-                agregarPelicula(laPelicula)
-            });
-        })
+    }
+    peliculas.forEach(laPelicula => {
+        document.getElementById(`boton${laPelicula.id}`).addEventListener("click", function () {
+            agregarPelicula(laPelicula)
+        });
+    })
 }
 
-let carrito=[];
+let carrito = [];
 
-function agregarPelicula(laPelicula){
+function agregarPelicula(laPelicula) {
     carrito.push(laPelicula);
-    Swal.fire('agregaste <strong>'+laPelicula.nombre+"</strong> a tu compra");
+    Swal.fire('agregaste <strong>' + laPelicula.nombre + "</strong> a tu compra");
 
 
-    document.getElementById("tablaCompras").innerHTML+=`
+    document.getElementById("tablaCompras").innerHTML += `
         <td>${laPelicula.id}</td>
         <td>${laPelicula.nombre}</td>
         <td>${laPelicula.precio}$</td>
     `;
 }
-
