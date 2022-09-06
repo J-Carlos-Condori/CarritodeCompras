0e034bf590cad90974ed8fff3b8b841c9e680279
carlos_ <carlos.condori1412@gmail.com>
2022-09-06 17:41:21 -0300

    usando api de peliculas

--- codigo.js
+++ codigo.js
@@ -1,98 +1,47 @@
-let peliculas = [
-    {
-        id: 1,
-        precio: 100,
-        generos: ["superheroes", "violencia", "accion", "serie"],
-        nombre: "WATCHMEN",
-        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada: "./portadas/img1-Watchmen.jpg",
-        enlace: "https://www.youtube.com/watch?v=ejjFCjFJBXM",
+mostrarapi();
+function mostrarapi() {
+    const urlapi = "https://api.themoviedb.org/3/movie/popular?api_key=8b80abc3eb76e7827412b2b29e6f484f";
+    fetch(urlapi)
+        .then(resultado => resultado.json())
+        .then(lpeliculas => {
 
-    },
-    {
-        id: 2,
-        precio: 200,
-        generos: ["serie", "catastrofes", "hechos reales"],
-        nombre: "CHERNOBYL",
-        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada: "./portadas/img2-Chernobyl.jpg",
-        enlace: "https://www.youtube.com/watch?v=s9APLXM9Ei8&t=31s",
-    },
-    {
-        id: 3,
-        precio: 150,
-        generos: ["animacion", "superheroes", "misterio"],
-        nombre: "BATMAN",
-        descripcion: "El equipo : la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada: "./portadas/img3-Batman.jpg",
-        enlace: "https://www.youtube.com/watch?v=rrmUk2YUm14",
-    },
-    {
-        id: 4,
-        precio: 190,
-        generos: ["ciencia ficcion", "drama", "serie", "accion"],
-        nombre: "WESTWORLD",
-        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada: "./portadas/img4-Westworld.jpg",
-        enlace: "https://www.youtube.com/watch?v=SV5lzrbNDx0",
-    },
-    {
-        id: 5,
-        precio: 220,
-        generos: ["animacion", "superheroes", "aventura"],
-        nombre: "SUPERMAN",
-        descripcion: "El equipo creativo de Batman: la serie animada, ganadora en los Emmy, describe las aventuras de Superman, legendario superhéroe, contra las fuerzas del mal y su lucha constante por la verdad, la justicia y el estilo de vida americano .",
-        portada: "./portadas/img5-Superman.jpg",
-        enlace: "https://www.youtube.com/watch?v=ZSPwGK-xLhk",
-    },
-];
+            const apipeliculas = lpeliculas.results;
+            console.log(lpeliculas.results);
 
-
-
-
-
-let contenedor = document.getElementById("listado");
-
-mostrar();
-
-function mostrar() {
-    for (const laPelicula of peliculas) {
-
-        let arraygeneros = laPelicula.generos.join(" - ");
-
-        contenedor.innerHTML += `<div class="col-lg-4 col-sm-6 col-12">
-                    <div class="card mt-3">
+            apipeliculas.forEach(apipelicula => {
+                document.querySelector("#listadoapi").innerHTML += `
+                <div class="col-lg-4 col-sm-6 col-12">
+                    <div class="card text-white bg-dark mt-3">
                         <div class="card-body">
-                            <h2 class="main__subtitulo card-tittle">${laPelicula.nombre}</h2>
+                            <h2 class="main__subtitulo card-tittle">${apipelicula.title}</h2>
                             <div class="main__parrafo">
-                                <p class="card-text">${laPelicula.descripcion}</p>
+                                <p class="card-text">${apipelicula.overview}</p>
                             </div>
                             <br>
-                            <img src=${laPelicula.portada} class="card-img" alt="pelicula">
-                            
+                            <img src="https://image.tmdb.org/t/p/w500/${apipelicula.poster_path}" class="card-img" alt="pelicula">
+
                             <div>
                                 <br>
-                                    <h2><strong>$${laPelicula.precio}</strong></h2>
+                                    <h2><strong>$${apipelicula.vote_count}</strong></h2>
                                 <br>
-                                    <button type="button" class="btn btn-outline-success" id="boton${laPelicula.id}"><a target>COMPRAR</a></button>
-                                    <a href="${laPelicula.enlace}" class="btn btn-outline-warning" target="_blank">VER TRAILER</a>
-                            </div>
-                            <div id="genero">
-                                <br>
-                                <h5><strong>${arraygeneros}<strong></h5>
+                                    <button type="button" class="btn btn-outline-success" id="boton${apipelicula.id}"><a target>COMPRAR</a></button>
+                                    <a href="${apipelicula.title}" class="btn btn-outline-warning" target="_blank">VER TRAILER</a>
                             </div>
                         </div>
             </div>`;
-    }
-    peliculas.forEach(laPelicula => {
-        document.getElementById(`boton${laPelicula.id}`).addEventListener("click", function () {
+
+            });
+        })
+
+    apipeliculas.forEach(laPelicula => {
+
+        console.log(lpeliculas.results);
+        document.getElementById(`boton${apipelicula.title}`).addEventListener("click", function () {
             agregarPelicula(laPelicula)
         });
     })
 }
-
 let carrito = [];
-
 function agregarPelicula(laPelicula) {
     carrito.push(laPelicula);
     Swal.fire('agregaste <strong>' + laPelicula.nombre + "</strong> a tu compra");
